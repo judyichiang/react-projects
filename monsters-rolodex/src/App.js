1c0e998723776ceed2e4e10a7626ed591fd7a493
Judy Chiang <jiyc1119@gmail.com>
2020-05-14 00:05:11 -0700

    componentDidMount

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -6,17 +6,23 @@ class App extends Component{
   constructor(){
     super();
     this.state = {
-      monsters: [
-        {name: 'Frankenstein'},
-        {name: 'Dracula'},
-        {name: 'Zombie'}
-      ]
+      monsters: [ ]
     }
   }
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(users => this.setState({monsters:users}))
+  }
+
+
 
   render(){
     return (
       <div className="App">
+      {this.state.monsters.map(monster => (<h1 key={monster.id}>{monster.name}</h1>))
+      }
 
       </div>
     )
