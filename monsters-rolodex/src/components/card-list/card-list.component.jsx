8ce8ca2af046374a9311cebb3ab2fc9e828cd83c
Judy Chiang <jiyc1119@gmail.com>
2020-05-17 14:48:27 -0700

    add monster arr to card-list

--- monsters-rolodex/src/components/card-list/card-list.component.jsx
+++ monsters-rolodex/src/components/card-list/card-list.component.jsx
@@ -2,9 +2,14 @@ import React from 'react';
 import './card-lis.styles.css'
 
 export const CardList = (props) => {
-  // console.log(props)
+  console.log(props)
   return (
   // <div><h1>{props.children}</h1></div>
-  <div className="card-list">{props.children}</div>
+  <div className="card-list">
+  {props.monsters.map(monster => (
+    <h1 key={monster.id}>{monster.name}</h1>
+  ))}
+
+  </div>
   )
 }
